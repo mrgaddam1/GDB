2cd58175ccdc57de05c28c5b9d19f6fc458d47b9
mrgaddam1 <mrgaddam@gmail.com>
2025-12-13 15:09:01 +0000

    Added FirstName in the View Model

--- GDB.Web/GDB.Web.DataAccess/Implementation/OrdersRepository.cs
+++ GDB.Web/GDB.Web.DataAccess/Implementation/OrdersRepository.cs
@@ -6,12 +6,6 @@ using GDB.Web.Shared;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace GDB.Web.DataAccess.Implementation
 {
